88334f5854cf41debe573e35a0ccef9475ea61e4
Sharan Srivatsa <shsr7814@colorado.edu>
2019-02-06 13:23:00 -0700

    setup basic components architecture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Todos from './components/Todos/Todos';
 class App extends Component {
-  render() {
+  
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render() {     
     return (
       <div className="App">
         <h1>App </h1>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
