18d8d47337c0fc9943a8815d4cf75247c8e88c8b
Gerard Casas Saez <casassg@gmail.com>
2019-04-05 18:24:36 -0600

    add auth for events actions

--- new-dashboard/src/actions/eventActions.js
+++ new-dashboard/src/actions/eventActions.js
@@ -1,52 +1,67 @@
 import { NEW_EVENT, FETCH_EVENTS, UPDATED_EVENT } from './types';
+import firebase from "firebase";
 
-
-export const fetchEvents = () => dispatch => {      
-    fetch('http://34.95.114.189/events/')
-    .then(res => res.json())
-    .then(myevents => dispatch({
-        type: FETCH_EVENTS,
-        payload: myevents
-    }))
-    .catch(function(error) {
-        console.log('There has been a problem with your fetch operation: ', error.message);
-      });;    
+export const fetchEvents = () => dispatch => {
+    firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(idToken => {
+        fetch('http://34.95.114.189/events/', {
+            headers: {
+                'Authorization': `Bearer ${idToken}`,
+            }
+        })
+            .then(res => res.json())
+            .then(myevents => dispatch({
+                type: FETCH_EVENTS,
+                payload: myevents
+            }))
+            .catch(function (error) {
+                console.log('There has been a problem with your fetch operation: ', error.message);
+            });;
+    });
 };
 
-export const createEvent = (eventData) => dispatch => {          
-    fetch('http://34.95.114.189/events', {
-        method: 'POST',        
-        headers : {
-            'content-type' : 'application/json'          
-        },
-        body: JSON.stringify(eventData)
-    })
-    .then(res => {
-        if (res.ok){
-            return res.json()
-        } else{
-            console.log("We need to handle this error");
-        }
-        
-    })    
-    .then(myevent => dispatch({
-        type: NEW_EVENT,
-        payload: myevent
-    })
-    );
+export const createEvent = (eventData) => dispatch => {
+    if (firebase.auth().currentUser == null) {
+        throw Error("Not authed")
+    }
+    firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(idToken => {
+        fetch('http://34.95.114.189/events', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${idToken}`
+            },
+            body: JSON.stringify(eventData)
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    console.log("We need to handle this error");
+                }
+
+            })
+            .then(myevent => dispatch({
+                type: NEW_EVENT,
+                payload: myevent
+            })
+            );
+    });
 };
 
-export const modifyEvents = (status, normalized_name) => dispatch => {         
-    fetch(`http://34.95.114.189/events/${normalized_name}/${status}`, {
-        method: 'PUT',        
-        headers : {
-            'content-type' : 'application/json'          
-        }        
-    }).then(res => res.json())
-    .then(updatedEvent => dispatch({
-        type: UPDATED_EVENT,
-        payload: updatedEvent
-    })
-    );
-        
+export const modifyEvents = (status, normalized_name) => dispatch => {
+    firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(idToken => {
+        fetch(`http://34.95.114.189/events/${normalized_name}/${status}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${idToken}`
+            }
+        }).then(res => res.json())
+            .then(updatedEvent => dispatch({
+                type: UPDATED_EVENT,
+                payload: updatedEvent
+            })
+            );
+    });
+
 };
