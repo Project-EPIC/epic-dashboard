7f0f198d732540ff4ec16992e15277bc5da2b449
Gerard Casas Saez <casassg@gmail.com>
2019-04-24 19:06:46 -0600

    separate event table component for easier composition of ListEvent

---
+++ dashboard/src/components/EventsAPI/ListEvents/EventTable/EventTable.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MaterialTable from 'material-table'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { modifyEvents } from "../../../../actions/eventActions";
+
+
+
+class EventTable extends React.Component {
+
+
+ 
+  render() {
+    const { history, title, data } = this.props;    
+
+    return   (      
+
+
+          <MaterialTable
+            columns={[
+              { title: "Name", field: 'name', },
+              
+              { title: 'Description', field: 'description' },
+              { title: 'Query', field: 'keywords', render: rowData => rowData.keywords.join(', ') },    
+            ]}
+            options={{ search: false, paging: false, actionsColumnIndex: -1, pageSize: 20, pageSizeOptions: [10, 20, 30] }}
+            data={data}
+            title={title}
+            onRowClick = {(event, rowData) => {
+              history.push(`/events/${rowData.normalized_name}/`)
+            }}
+            actions={[
+              rowData=>({
+                  icon: rowData.status === "ACTIVE"? 'pause':'play_arrow',
+                  tooltip: rowData.status === "ACTIVE"? 'Stop collection':'Restart collection',
+                  onClick: (event, rowData) => {
+                    this.props.modifyEvents(rowData.status === "ACTIVE" ? "NOT_ACTIVE" : "ACTIVE",rowData.normalized_name)
+                  },
+              }),
+              {
+                icon:"add",
+                tooltip:"Event details",
+                onClick: (event, rowData) => {
+                  history.push(`/events/${rowData.normalized_name}/`)
+                },
+              }
+            ]
+            }
+          />
+       
+    );
+  }
+}
+
+EventTable.propTypes = {
+  title:  PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+};
+
+const mapDispatchToProps = {
+  modifyEvents: modifyEvents,
+}
+
+
+export default connect(null, mapDispatchToProps)(withRouter(EventTable));
