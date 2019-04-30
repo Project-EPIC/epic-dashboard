6281091ec26b80de5287be930ec31c7927ca8259
Gerard Casas Saez <casassg@gmail.com>
2019-04-29 19:36:15 -0600

    add activity and author

--- dashboard/src/components/EventsAPI/EventDashboard/EventDashboard.js
+++ dashboard/src/components/EventsAPI/EventDashboard/EventDashboard.js
@@ -4,16 +4,18 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { styles } from "./styles";
 import { connect } from 'react-redux';
-import { fetchEvent, fetchCounts,modifyEvents } from "../../../actions/eventActions";
+import { fetchEvent, fetchCounts, modifyEvents } from "../../../actions/eventActions";
 import PauseIcon from "@material-ui/icons/Pause"
 import OpenInNewIcon from "@material-ui/icons/OpenInNew"
 import PlayArrowIcon from "@material-ui/icons/PlayArrow"
+import CreateIcon from "@material-ui/icons/Create"
+import ReactTimeAgo from 'react-time-ago'
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { LinearProgress, List, ListItem, ListItemText, Paper } from '@material-ui/core';
+import { LinearProgress, List, ListItem, ListItemText, ListItemIcon, Paper,Tooltip } from '@material-ui/core';
 import TweetsChart from '../TweetsChart/TweetsChart';
 
 
@@ -25,6 +27,31 @@ class EventDashboard extends React.Component {
 
     }
 
+    renderIconStatus(status) {
+        switch(status){
+            case "START_EVENT":
+                return <Tooltip title="Event started" aria-label="Event started"><PlayArrowIcon/></Tooltip>
+            case "PAUSE_EVENT":
+                return <Tooltip title="Event paused" aria-label="Event paused"><PauseIcon/></Tooltip>
+            case "CREATE_EVENT":
+                return <Tooltip title="Event created" aria-label="Event created"><CreateIcon/></Tooltip>
+            default:
+                return <OpenInNewIcon/>
+        }
+    }
+
+    renderText(status) {
+        switch(status){
+            case "START_EVENT":
+                return "Collection started"
+            case "PAUSE_EVENT":
+                return "Collection paused"
+            case "CREATE_EVENT":
+                return "Event created"
+            default:
+                return "Activity"
+        }
+    }
 
     render() {
         const rows = this.props.events;
@@ -33,118 +60,149 @@ class EventDashboard extends React.Component {
         const event = rows.find((o) => {
             return o.normalized_name === eventId;
         });
-        const totalCount = (this.props.counts[eventId] && this.props.counts[eventId].map((item)=> item.count).reduce((x,y)=> x+y))||0;
+        const totalCount = (this.props.counts[eventId] && this.props.counts[eventId].map((item) => item.count).reduce((x, y) => x + y)) || 0;
 
-        
+
 
         if (event !== undefined)
             return (
-
                 <Grid container spacing={24}>
+                    <Grid item md={8} xs={12} >
+                        <Grid container spacing={24}>
 
-                    <Grid item md={3} xs={12} >
-                        <Card className={classes.card}>
-                            <CardContent>
-                            <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    Event details
+                            <Grid item md={4} xs={12} >
+                                <Card className={classes.card}>
+                                    <CardContent>
+                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                            Event details
                                 </Typography>
-                                <Typography variant="h5" component="h5">
-                                    {event.name}
+                                        <Typography variant="h5" component="h5">
+                                            {event.name}  (<i>{event.normalized_name}</i>)
 
-                                </Typography>
-                                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    Normalized name: <i>{event.normalized_name}</i>
-                                </Typography>
-                                <Typography variant="body1">
-                                    {event.description}
-                                </Typography>
-                                <Typography variant="body2" gutterBottom color="textSecondary" align="right" >
-                                    <i>Created: {event.created_at_str}</i>
-                                </Typography>
+                                        </Typography>
+                                        
+                                        <Typography variant="body1">
+                                            {event.description}
+                                        </Typography>
+                                        <Typography variant="caption" gutterBottom color="textSecondary" >
+                                            <i><ReactTimeAgo date={event.created_at}/> by {event.author}</i>
+                                        </Typography>
 
 
-                            </CardContent>
+                                    </CardContent>
 
-                        </Card>
+                                </Card>
+                            </Grid>
+                            <Grid item md={4} xs={12}>
+                                <Card className={classes.card}>
+                                    <CardContent>
+                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                            Keywords for collection
+                                </Typography>
+
+                                        <List dense>
+                                            {event.keywords.map(keyword =>
+                                                <ListItem key={keyword}>
+                                                    <ListItemText primary={keyword} />
+                                                </ListItem>
+                                            )}
+                                        </List>
+                                    </CardContent>
+
+                                </Card>
+                            </Grid>
+                            <Grid item md={4} xs={12}>
+                                <Card className={classes.card}>
+                                    <CardContent>
+                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                            Tweets collected
+                                        </Typography>
+                                        <Typography variant="h2" component="h2">
+                                            {totalCount}
+
+                                        </Typography>
+
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button color="default" className={classes.button} target="_blank" component="a" href={event.big_query_table_url}>
+                                            Explore in BigQuery
+                                        <OpenInNewIcon className={classes.rightIcon} />
+                                        </Button>
+
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                            
+                            <Grid item xs={12} >
+                                <Paper className={classes.chartPaper}>
+                                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                        Tweets per hour
+                                </Typography>
+                                    <TweetsChart annotateEvent={event.normalized_name} updateTimePeriod={(d) => (d)} />
+                                </Paper>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    <Grid item md={3} xs={12}>
+                    <Grid item md={4} xs={12} >
+                    <Grid container spacing={24}>
+                    <Grid item xs={12} >
+                                <Card className={classes.card}>
+                                    <CardContent>
+                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                            Status
+                                        </Typography>
+                                        <Typography variant="h5" component="h5">
+                                            {event.status === "ACTIVE" ? "Collecting data" : (event.status === "FAILED" ? "Collection failed!" : "Not collecting new data")}
+                                        </Typography>
+
+
+                                        {event.status === "ACTIVE" ?
+                                            <LinearProgress />
+                                            :
+                                            null
+                                        }
+                                    </CardContent>
+                                    <CardActions>
+                                        {event.status === "ACTIVE" ?
+                                            <Button color="default" className={classes.button} onClick={() => this.props.modifyEvents("NOT_ACTIVE", event.normalized_name)}>
+                                                Pause collection
+                                        <PauseIcon className={classes.rightIcon} />
+                                            </Button>
+                                            :
+                                            <Button color="default" className={classes.button} onClick={() => this.props.modifyEvents("ACTIVE", event.normalized_name)}>
+                                                Restart collection
+                                        <PlayArrowIcon className={classes.rightIcon} />
+                                            </Button>
+                                        }
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                            <Grid item  xs={12} >
                         <Card className={classes.card}>
                             <CardContent>
-                            <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                Keywords for collection
-                                </Typography>
-                                
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Event activity log
+                            </Typography>
+
                                 <List dense>
-                                    {event.keywords.map(keyword =>
-                                        <ListItem key={keyword}>
-                                            <ListItemText primary={keyword} />
+                                    {event.activity && event.activity.map(activity =>
+                                        <ListItem key={activity.time}>
+                                            <ListItemIcon>
+                                               {this.renderIconStatus(activity.type)}
+                                            </ListItemIcon>
+                                            <ListItemText primary={this.renderText(activity.type)} secondary={
+                                                <React.Fragment>
+                                                    <ReactTimeAgo date={activity.time} /> by {activity.author}
+                                                </React.Fragment>
+                                            } />
                                         </ListItem>
                                     )}
                                 </List>
                             </CardContent>
 
                         </Card>
+                        </Grid>
                     </Grid>
-                    <Grid item md={3} xs={12}>
-                        <Card className={classes.card}>
-                            <CardContent>
-                            <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                Tweets collected
-                                </Typography>
-                                <Typography variant="h2" component="h2">
-                                    {totalCount}
-
-                                </Typography>
-                                
-                            </CardContent>
-                            <CardActions>
-                            <Button color="default" className={classes.button} target="_blank" component="a" href={event.big_query_table_url}>
-                                        Explore in BigQuery
-                                        <OpenInNewIcon className={classes.rightIcon} />
-                                    </Button>
-                                
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item md={3} xs={12} >
-                        <Card className={classes.card}>
-                            <CardContent>
-                            <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                Status
-                                </Typography>
-                                <Typography variant="h5" component="h5">
-                                    {event.status==="ACTIVE"?"Collecting data":(event.status==="FAILED"? "Collection failed!":"Not collecting new data")}
-                                </Typography>
-
-
-                                {event.status === "ACTIVE" ?
-                                    <LinearProgress />
-                                    :
-                                    null
-                                }
-                            </CardContent>
-                            <CardActions>
-                                {event.status === "ACTIVE" ?
-                                    <Button color="default" className={classes.button} onClick={()=> this.props.modifyEvents("NOT_ACTIVE",event.normalized_name)}>
-                                        Pause collection
-                                        <PauseIcon className={classes.rightIcon} />
-                                    </Button>
-                                    :
-                                    <Button color="default" className={classes.button} onClick={()=> this.props.modifyEvents("ACTIVE",event.normalized_name)}>
-                                        Restart collection
-                                        <PlayArrowIcon className={classes.rightIcon} />
-                                    </Button>
-                                }
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} >
-                        <Paper className={classes.chartPaper}>
-                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                Tweets per hour
-                                </Typography>
-                            <TweetsChart annotateEvent={event.normalized_name} updateTimePeriod={(d)=>(d)} />
-                        </Paper>
                     </Grid>
                 </Grid>
 
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import JavascriptTimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+JavascriptTimeAgo.locale(en)
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
