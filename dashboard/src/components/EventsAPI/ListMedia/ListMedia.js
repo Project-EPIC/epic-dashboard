ace306d04b53e76cb8459bb375f133067abbf963
Ismael Garrido <100346215@alumnos.uc3m.es>
2019-06-11 17:34:11 -0600

    infinite scroll and error image added

--- dashboard/src/components/EventsAPI/ListMedia/ListMedia.js
+++ dashboard/src/components/EventsAPI/ListMedia/ListMedia.js
@@ -11,9 +11,15 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
 
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
+import { defaultErrorImage } from './errorBase64';
+
+
+let pageCounter = 1;
 
 class ListMedia extends Component {
 
@@ -21,6 +27,9 @@ class ListMedia extends Component {
     super(props);
 
     this.state = {
+      error: false,
+      hasMore: true,
+      isLoading: false,
       photoIndex: 0,
       isOpen: false,
       data: null,
@@ -30,26 +39,79 @@ class ListMedia extends Component {
 
   componentDidMount() {
 
+    window.addEventListener('scroll', this.handleScroll);
     const { eventId } = this.props;
+    pageCounter = 1;
 
-    fetch(`http://localhost:8080/media/${eventId}/?page=1&count=50`)
+    fetch(`http://localhost:8080/media/${eventId}/?page=1&count=51`)
       .then(response => response.json())
       .then(data => this.setState({ media: data.media }));
   }
 
-  onScroll = () => {
+  handleScroll = () => {
+    //console.log("ENTER HANDLE SCROLL\n");
+    const {
+      loadImages,
+      state: {
+        error,
+        isLoading,
+        hasMore,
+      },
+    } = this;
+
+    // Bails early if:
+    // * there's an error
+    // * it's already loading
+    // * there's nothing left to load
+    if (error || isLoading || !hasMore) return;
+
+    // console.log("WINDOW INNER HEIGHT: " + window.innerHeight + "\n");
+    // console.log("DOCUMENT ELEMENT SCROLL TOP: " + document.documentElement.scrollTop + "\n");
+    // console.log("DOCUMENT ELEMENT OFFSET HEIGHT: " + document.documentElement.offsetHeight + "\n");
+    // console.log("INNER HEIGHT + SCROLL TOP: " + (window.innerHeight + document.documentElement.scrollTop) + "\n");
+
     if (
-      (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) &&
-      this.props.list.length
+      window.innerHeight + document.documentElement.scrollTop
+      >= document.documentElement.offsetHeight
     ) {
-      this.props.onPaginatedSearch();
-      console.log("keloke chavales");
+      console.log("ENTER LOAD IMAGES\n");
+      loadImages();
     }
+  };
+
+  loadImages = () => {
+    const { eventId } = this.props;
+    pageCounter++;
+    console.log("PAGE COUNTER: " + pageCounter +"\n");
+
+    this.setState({ isLoading: true }, () => {
+        fetch(`http://localhost:8080/media/${eventId}/?page=${pageCounter}&count=51`)
+        .then(response => response.json())
+        .then(
+            data => {
+            console.log("STATE MEDIA LENGHT: " + this.state.media.length + "\n");
+            this.setState(state => ({
+                hasMore: (state.media.length >= 1),
+                isLoading: false,
+                media: [...state.media, ...data.media],
+            }))
+            })
+            .catch((err) => {
+            this.setState({
+                error: err.message,
+                isLoading: false,
+            });
+            })
+    });
+  }
+
+  addDefaultSrc(ev) {
+    ev.target.src = defaultErrorImage
   }
 
   render() {
     const { eventId } = this.props;
-    const { media, lightbox, photoIndex, isOpen } = this.state;
+    const { error, hasMore, isLoading, media, lightbox, photoIndex, isOpen } = this.state;
 
     return (
       <Grid container spacing={24}>
@@ -57,7 +119,7 @@ class ListMedia extends Component {
           <GridList cellHeight={300} width={100} height={100} cols={3}>
             {media.map((media, index) => (
               <GridListTile key={media.image_link} cols={1}>
-                <img onClick={()=> this.setState({lightbox: true, photoIndex:index})} src={media.image_link} alt={media.tweet_url} />
+                <img onError={this.addDefaultSrc} style={{width: '100%'}} onClick={()=> this.setState({lightbox: true, photoIndex:index})} src={media.image_link} alt={media.tweet_url} />
                 <a href={"https://www.twitter.com/" + media.user} target="_blank">
                   <GridListTileBar
                     title={"@" + media.user}
@@ -74,6 +136,21 @@ class ListMedia extends Component {
               </GridListTile>
             ))}
           </GridList>
+          <div>
+            {error &&
+            <div style={{ color: '#900' }}>
+              {error}
+            </div>
+            }
+            {isLoading &&
+              <div style={{ textAlign: 'center' , marginTop: '2%' }}>
+                <CircularProgress />
+              </div>
+            }
+            {!hasMore &&
+              <Typography style={{ fontSize: '150%', fontWeight: 'bold', textAlign: 'center' , marginTop: '2%' }}>No more elements to load</Typography>
+            }
+          </div>
 
           {lightbox && (
             <Lightbox
